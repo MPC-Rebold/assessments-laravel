0e9ca393ce64fd316dfb5537859ce62e733d554e
Andrew Wang <awwang3@uci.edu>
2024-02-15 17:15:55 -0800

    chore: update README

--- database/seeders/SettingsSeeder.php
+++ database/seeders/SettingsSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Settings;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class SettingsSeeder extends Seeder
