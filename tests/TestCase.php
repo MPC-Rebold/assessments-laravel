6b643b1ed75ee76b32c650257887166e73ad59da
Andrew Wang <awwang3@uci.edu>
2024-06-01 19:19:49 -0700

    delete Example Course

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,10 +9,20 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 
+    private array $seedDirectory;
+
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->seed(SettingsSeeder::class);
+    }
+
+    // run this after finishing all tests
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        echo '';
     }
 }
